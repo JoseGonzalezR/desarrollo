f13251369c8dbc905e57505115fa1c21f8760515
nicanorpushainaa <nicanorpushaina77@gmail.com>
2025-05-30 13:10:08 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,3 +13,26 @@ document.getElementById("form-contacto").addEventListener("submit", function (e)
 
     this.reset();
 });
+
+    const cards = document.querySelectorAll(".slide-up");
+    const showOnScroll = () => {
+      cards.forEach(card => {
+        const rect = card.getBoundingClientRect();
+        if (rect.top <= window.innerHeight - 100) {
+          card.classList.add("visible");
+        }
+      });
+    };
+    window.addEventListener("scroll", showOnScroll);
+    showOnScroll();
+
+    
+    document.getElementById("contact-form").addEventListener("submit", function (e) {
+      e.preventDefault();
+      const mensaje = document.getElementById("mensaje-exito");
+      mensaje.style.display = "block";
+      setTimeout(() => {
+        mensaje.style.display = "none";
+        this.reset();
+      }, 3000);
+    });
